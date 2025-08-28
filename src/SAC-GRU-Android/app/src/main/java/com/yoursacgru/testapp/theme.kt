17beccc3fcbf7400d6ba6f7cf8360abb70932914
sudhindra-a700 <prakashsudhindra@gmail.com>
2025-08-28 23:14:38 +0530

    Create theme.kt

---
+++ src/SAC-GRU-Android/app/src/main/java/com/yoursacgru/testapp/theme.kt
@@ -0,0 +1,11 @@
+package com.yoursacgru.testapp
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SACGRUTheme(content: @Composable () -> Unit) {
+    MaterialTheme {
+        content()
+    }
+}
