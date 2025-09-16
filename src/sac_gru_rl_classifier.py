5af057f821d995f001b8a34c2c7a07e5b30c3017
sudhindra-a700 <prakashsudhindra@gmail.com>
2025-09-16 14:47:13 +0530

    Update sac_gru_rl_classifier.py

--- src/sac_gru_rl_classifier.py
+++ src/sac_gru_rl_classifier.py
@@ -9,7 +9,6 @@ import random
 logger = logging.getLogger(__name__)
 
 class GRUEncoder(layers.Layer):
-    """GRU-based sequence encoder for traffic features."""
     def __init__(self, hidden_units=64, dropout_rate=0.2, **kwargs):
         super(GRUEncoder, self).__init__(**kwargs)
         self.gru1 = layers.GRU(hidden_units, return_sequences=True, dropout=dropout_rate)
@@ -22,8 +21,7 @@ class GRUEncoder(layers.Layer):
         return self.batch_norm(x, training=training)
 
 class SACActorNetwork(Model):
-    """SAC Actor Network - The policy model for classification."""
-    def __init__(self, state_dim, action_dim=2, hidden_units=64):
+    def __init__(self, state_dim, action_dim=4, hidden_units=64):
         super(SACActorNetwork, self).__init__()
         self.gru_encoder = GRUEncoder(hidden_units)
         self.dense1 = layers.Dense(256, activation='relu')
@@ -45,8 +43,7 @@ class SACActorNetwork(Model):
         return action, log_prob
 
 class SACCriticNetwork(Model):
-    """SAC Critic Network - Q-value estimation."""
-    def __init__(self, state_dim, action_dim=2, hidden_units=64):
+    def __init__(self, state_dim, action_dim=4, hidden_units=64):
         super(SACCriticNetwork, self).__init__()
         self.gru_encoder = GRUEncoder(hidden_units)
         self.dense1 = layers.Dense(256, activation='relu')
@@ -60,7 +57,6 @@ class SACCriticNetwork(Model):
         return self.q_values(x)
 
 class ReplayBuffer:
-    """Experience replay buffer for SAC training."""
     def __init__(self, capacity=100000):
         self.buffer = deque(maxlen=capacity)
     
@@ -76,24 +72,23 @@ class ReplayBuffer:
         return len(self.buffer)
 
 class SACGRUClassifier:
-    """Full SAC-GRU Reinforcement Learning Classifier."""
     
-    def __init__(self, sequence_length=1, feature_dim=7, hidden_units=64):
+    def __init__(self, sequence_length=1, feature_dim=7, hidden_units=64, action_dim=4):
         self.sequence_length = sequence_length
         self.feature_dim = feature_dim
+        self.action_dim = action_dim
         self.lr = 3e-4
         self.gamma = 0.99
         self.tau = 0.005
         self.is_trained = False
         
         state_dim = self.sequence_length * self.feature_dim
-        self.actor = SACActorNetwork(state_dim, hidden_units=hidden_units)
-        self.critic1 = SACCriticNetwork(state_dim, hidden_units=hidden_units)
-        self.critic2 = SACCriticNetwork(state_dim, hidden_units=hidden_units)
-        self.target_critic1 = SACCriticNetwork(state_dim, hidden_units=hidden_units)
-        self.target_critic2 = SACCriticNetwork(state_dim, hidden_units=hidden_units)
+        self.actor = SACActorNetwork(state_dim, action_dim=self.action_dim, hidden_units=hidden_units)
+        self.critic1 = SACCriticNetwork(state_dim, action_dim=self.action_dim, hidden_units=hidden_units)
+        self.critic2 = SACCriticNetwork(state_dim, action_dim=self.action_dim, hidden_units=hidden_units)
+        self.target_critic1 = SACCriticNetwork(state_dim, action_dim=self.action_dim, hidden_units=hidden_units)
+        self.target_critic2 = SACCriticNetwork(state_dim, action_dim=self.action_dim, hidden_units=hidden_units)
 
-        # Initialize target networks
         dummy_input = tf.random.normal((1, self.sequence_length, self.feature_dim))
         self.critic1(dummy_input)
         self.target_critic1.set_weights(self.critic1.get_weights())
@@ -107,9 +102,6 @@ class SACGRUClassifier:
         self.replay_buffer = ReplayBuffer()
 
     def train(self, features, labels, num_episodes, batch_size=64):
-        """
-        Pre-trains the RL agent using a labeled dataset.
-        """
         logger.info(f"Starting supervised pre-training for {num_episodes} episodes...")
 
         for episode in range(num_episodes):
@@ -122,7 +114,6 @@ class SACGRUClassifier:
             action, _ = self.actor.sample_action(state)
             action_val = int(action.numpy()[0])
             
-            # Use the label to generate a reward
             reward = 1.0 if action_val == true_label else -1.0
             
             self.replay_buffer.push(state[0], action_val, reward, state[0], True)
@@ -137,7 +128,6 @@ class SACGRUClassifier:
         logger.info("Supervised pre-training complete!")
 
     def _update_networks(self, batch_size):
-        """Performs one step of SAC update."""
         states, actions, rewards, next_states, dones = self.replay_buffer.sample(batch_size)
         
         states = tf.convert_to_tensor(states, dtype=tf.float32)
@@ -147,7 +137,6 @@ class SACGRUClassifier:
         dones = tf.convert_to_tensor(dones, dtype=tf.float32)
 
         with tf.GradientTape(persistent=True) as tape:
-            # Critic Loss
             next_actions, _ = self.actor.sample_action(next_states)
             target_q1 = self.target_critic1(next_states)
             target_q2 = self.target_critic2(next_states)
@@ -163,7 +152,6 @@ class SACGRUClassifier:
             q2_selected = tf.gather(q2_values, actions, batch_dims=1)
             critic2_loss = tf.reduce_mean(tf.square(tf.squeeze(q2_selected, axis=1) - target_values))
 
-            # Actor Loss
             actions_pred, log_probs = self.actor.sample_action(states)
             q1_pred = self.critic1(states)
             q2_pred = self.critic2(states)
@@ -183,14 +171,12 @@ class SACGRUClassifier:
         self._soft_update_targets()
 
     def _soft_update_targets(self):
-        """Softly updates the weights of the target networks."""
         for target, source in zip(self.target_critic1.variables, self.critic1.variables):
             target.assign(self.tau * source + (1.0 - self.tau) * target)
         for target, source in zip(self.target_critic2.variables, self.critic2.variables):
             target.assign(self.tau * source + (1.0 - self.tau) * target)
 
     def export_actor_for_deployment(self, export_path):
-        """Exports the trained actor network to a TFLite file."""
         if not self.is_trained:
             raise ValueError("Model must be trained before export")
         
